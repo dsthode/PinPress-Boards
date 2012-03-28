45077f5cee770e3503456f8ea5096812752008c0
Damian Serrano Thode <dsthode@gmail.com>
2012-03-28 23:49:43 +0200

    Changed license text
    Changed license text to GNU GPLv2 to comply with WordPress licensing
    restrictions

--- pinpress_boards.php
+++ pinpress_boards.php
@@ -5,24 +5,26 @@ Plugin URI: http://dsthode.info/pinpress-boards/
 Description: Pins and boards for your Wordpress blog just like Pinterest
 Version: 0.42
 Author: Damian Serrano Thode
-Author URI: http://dsthode.info
-License: Apache License 2.0
+Author URI: http://dsthode.info/pinpress-boards/
+License: GNU GPLv2
 */
 
 /*
-Copyright 2021 Damian Serrano Thode
+Copyright (C) 2012  Damian Serrano Thode
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-       http://www.apache.org/licenses/LICENSE-2.0
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 /* Custom post types (pinboard and pins like) */
